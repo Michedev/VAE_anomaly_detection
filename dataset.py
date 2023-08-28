5a362fa6e761692c2cb490b8bcc4490b879bd802
mikedev <mik3dev@gmail.com>
2023-08-28 18:24:35 +0200

    add mnist dataset

--- dataset.py
+++ dataset.py
@@ -1,6 +1,20 @@
+from typing import Tuple
 import torch
 from torch.utils.data import Dataset, TensorDataset
-
+from torchvision.datasets import MNIST
 
 def rand_dataset(num_rows=60_000, num_columns=100) -> Dataset:
     return TensorDataset(torch.rand(num_rows, num_columns))
+
+
+def mnist_dataset(train=True) -> Dataset:
+    """
+    Returns the MNIST dataset for training or testing.
+    
+    Args:
+    train (bool): If True, returns the training dataset. Otherwise, returns the testing dataset.
+    
+    Returns:
+    Dataset: The MNIST dataset.
+    """
+    return MNIST(root='./data', train=train, download=True, transform=None)
